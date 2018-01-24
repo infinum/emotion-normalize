95fc5ce9db6a91dc496c4da71cc0a375fadba386
dingo-d <denis.zoljom@gmail.com>
2018-01-24 12:01:26 +0100

    Fixes from PR review

--- bin/build.js
+++ bin/build.js
@@ -1,34 +1,26 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
+
 
 // Read version from the normalize.css package and write it in our package.json
 const normalizePackage = JSON.parse(fs.readFileSync('./node_modules/normalize.css/package.json', 'utf8'));
 
 const normalizeVersion = normalizePackage.version;
 
-fs.readFile('./package.json', 'utf8', function (error, data) {
-  if (error) {
-    return console.log(error);
-  }
-  var result = data.replace(/("version": "[\d+\.]*")/g, `"version": "${normalizeVersion}"`);
+const data = fs.readFileSync('./package.json', 'utf8');
+const package = JSON.parse(data);
+package.version = normalizeVersion;
 
-  fs.writeFile('./package.json', result, 'utf8', function (error) {
-     if (error) {
-      return console.log(error);
-    };
-  });
-});
+fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
 
 // Read css from normalize.css, clean comments and write it in our index.js
-const readStream = fs.readFileSync(`./node_modules/normalize.css/${normalizePackage.main}`, 'utf8');
+const normalizeMain = path.join('./node_modules/normalize.css', normalizePackage.main);
+const readStream = fs.readFileSync(normalizeMain, 'utf8');
 const regex = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm;
 const normalize = readStream.replace(regex, '').replace(/^\s*\n/gm, '');
 
 const contents = `import {css} from 'emotion';\n\nconst normalize = css\`\n${normalize}\`;\n\nexport default normalize;`;
 
-fs.writeFile('./index.js', contents, (error) => {
-  if (error) {
-    return console.log(error);
-  }
-});
+fs.writeFileSync('./index.js', contents);
