3c3201a67f179b6527372c3a0eebc0345bd3f56a
dingo-d <denis.zoljom@gmail.com>
2018-01-24 09:17:39 +0100

    Fix build process

--- bin/build.js
+++ bin/build.js
@@ -2,47 +2,39 @@
 
 const fs = require('fs');
 
-let original = '';
 let version = '';
 
 // Read version from the normalize.css package and write it in our package.json
-const normalizePackage = fs.createReadStream('./node_modules/normalize.css/package.json', 'utf8');
+const normalizePackage = fs.readFileSync('./node_modules/normalize.css/package.json', 'utf8');
 
-normalizePackage.on('data', (chunk) => {
-  if (chunk.indexOf("version: ")) {
-    const match = chunk.match(/("version": "[\d+\.]*")/g)[0];
-    version = match.match(/[\d\.*]+/g)[0];
+if (normalizePackage.indexOf("version: ")) {
+  const match = normalizePackage.match(/("version": "[\d+\.]*")/g)[0];
+  version = match.match(/[\d\.*]+/g)[0];
+}
+
+fs.readFile('./package.json', 'utf8', function (error, data) {
+  if (error) {
+    return console.log(error);
   }
-}).on('end', () => {
-  fs.readFile('./package.json', 'utf8', function (error, data) {
-    if (error) {
-      return console.log(error);
-    }
-    var result = data.replace(/("version": "[\d+\.]*")/g, `"version": "${version}"`);
+  var result = data.replace(/("version": "[\d+\.]*")/g, `"version": "${version}"`);
 
-    fs.writeFile('./package.json', result, 'utf8', function (error) {
-       if (error) {
-        return console.log(error);
-      };
-    });
+  fs.writeFile('./package.json', result, 'utf8', function (error) {
+     if (error) {
+      return console.log(error);
+    };
   });
 });
 
 // Read css from normalize.css, clean comments and write it in our index.js
-const readStream = fs.createReadStream('./node_modules/normalize.css/normalize.css', 'utf8');
+const readStream = fs.readFileSync('./node_modules/normalize.css/normalize.css', 'utf8');
 
-readStream.on('data', (chunk) => {
-    original += chunk;
-    version
-  }).on('end', () => {
-    const regex = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm;
-    const normalize = original.replace(regex, '').replace(/^\s*\n/gm, '');
+const regex = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm;
+const normalize = readStream.replace(regex, '').replace(/^\s*\n/gm, '');
 
-    const contents = `import {css} from 'emotion';\n\nconst normalize = css\`${normalize}\`;\n\nexport default normalize;`;
+const contents = `import {css} from 'emotion';\n\nconst normalize = css\`${normalize}\`;\n\nexport default normalize;`;
 
-    fs.writeFile('./index.js', contents, (error) => {
-      if (error) {
-        return console.log(error);
-      }
-    });
+fs.writeFile('./index.js', contents, (error) => {
+  if (error) {
+    return console.log(error);
+  }
 });
