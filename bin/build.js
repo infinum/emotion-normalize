979fc33480d6561aade81c03426768c8bed06476
Ivica Batinic <ivica.batinic@infinum.hr>
2018-01-25 12:24:16 +0100

    Update build process

--- bin/build.js
+++ bin/build.js
@@ -7,11 +7,11 @@ const path = require('path');
 // Read version from the normalize.css package and write it in our package.json
 const normalizePackage = JSON.parse(fs.readFileSync('./node_modules/normalize.css/package.json', 'utf8'));
 
-const normalizeVersion = normalizePackage.version;
+// const normalizeVersion = normalizePackage.version;
 
 const data = fs.readFileSync('./package.json', 'utf8');
 const package = JSON.parse(data);
-package.version = normalizeVersion;
+// package.version = normalizeVersion;
 
 fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
 
@@ -23,4 +23,4 @@ const normalize = readStream.replace(regex, '').replace(/^\s*\n/gm, '');
 
 const contents = `import {css} from 'emotion';\n\nconst normalize = css\`\n${normalize}\`;\n\nexport default normalize;`;
 
-fs.writeFileSync('./index.js', contents);
+fs.writeFileSync('./src/index.js', contents);
