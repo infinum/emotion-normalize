05ecfaef26a083ab9adb88ea310be72e0c54850c
Ivica Batinic <ivica.batinic@infinum.hr>
2019-07-08 19:52:50 +0200

    Update build script

--- bin/build.js
+++ bin/build.js
@@ -3,24 +3,18 @@
 const fs = require('fs');
 const path = require('path');
 
-
-// Read version from the normalize.css package and write it in our package.json
 const normalizePackage = JSON.parse(fs.readFileSync('./node_modules/normalize.css/package.json', 'utf8'));
 
-// const normalizeVersion = normalizePackage.version;
-
-const data = fs.readFileSync('./package.json', 'utf8');
-const package = JSON.parse(data);
-// package.version = normalizeVersion;
-
-fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
-
-// Read css from normalize.css, clean comments and write it in our index.js
 const normalizeMain = path.join('./node_modules/normalize.css', normalizePackage.main);
 const readStream = fs.readFileSync(normalizeMain, 'utf8');
 const regex = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm;
 const normalize = readStream.replace(regex, '').replace(/^\s*\n/gm, '');
 
-const contents = `import {css} from '@emotion/core';\n\nconst normalize = css\`\n${normalize}\`;\n\nexport default normalize;`;
+const contents = `import { css } from '@emotion/core';
+
+const normalize = css\`
+${normalize}\`;
+
+export default normalize;`;
 
 fs.writeFileSync('./src/index.js', contents);
